6f62a9dbef4db810b0d0f4d549138a5a244f96da
gkandale <gkandale@cisco.com>
2017-09-05 17:49:58 -0400

    All Java Files

---
+++ src/main/java/pact/selenium/chapter3_9/pageobjects/Google.java
@@ -0,0 +1,31 @@
+package pact.selenium.chapter3_9.pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * Created by Ripon on 11/19/2015.
+ */
+public class Google {
+    private WebDriver driver;
+    private String baseURL;
+
+    public Google(WebDriver driver){
+        this.driver = driver;
+        baseURL = "https://www.google.ca/";
+        driver.get(baseURL + "?gfe_rd=cr&dcr=0&ei=0wGvWZe4E5L-8Ae7lpDAAQ&gws_rd=ssl");
+        System.out.println(driver.getTitle());
+        if (!driver.getTitle().equals("Google")){
+            throw new WrongPageException("Incorrect page for Google Home page");
+        }
+    }
+    public GoogleSearchPage goToSearchPage(){
+        driver.findElement(By.id("lst-ib")).sendKeys("Mastering Selenium Testing Tools");
+        driver.findElement(By.name("btnK")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("resultStats")));
+        return new GoogleSearchPage(driver);
+    }
+}
